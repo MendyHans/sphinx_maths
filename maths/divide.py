c3c48eca10a064725e2decd6dfadb2b25f2e304d
MendyHans <mendy.hans2@gmail.com>
2025-03-03 14:10:41 +0200

    added docstrings in py files and reran build

--- maths/divide.py
+++ maths/divide.py
@@ -1,4 +1,12 @@
 def divide_nums(num1, num2):
+    """This method will be used to divide two numbers
 
+        :param num1: The first number
+        :param num2: The second number (non zero number)
+
+        :returns: The quotient of two numbers
+
+        :rtype: real number
+    """
     answer = num1 / num2
     return answer
